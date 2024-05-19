4585a8ccaefc7e43fd67da17a4bcc45e6b3e26c1
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-05-20 03:37:53 +0500

    db

--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -1,8 +1,26 @@
 import 'package:bruhmius/components/my_receipt.dart';
+import 'package:bruhmius/models/restaurant.dart';
+import 'package:bruhmius/services/database/firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class DeliveryProgressPage extends StatelessWidget {
+class DeliveryProgressPage extends StatefulWidget {
   const DeliveryProgressPage({super.key});
+
+  @override
+  State<DeliveryProgressPage> createState() => _DeliveryProgressPageState();
+}
+
+class _DeliveryProgressPageState extends State<DeliveryProgressPage> {
+  FireStoreService db = FireStoreService();
+
+  @override
+  void initState() {
+    super.initState();
+
+    String receipt = context.read<Restaurant>().displayCartReceipt();
+    db.saveOrderToDatabase(receipt);
+  }
 
   @override
   Widget build(BuildContext context) {
