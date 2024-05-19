b97887da4ae06e656527d52aba7d1560c6c52d0b
M-Ali-Haider <mah01134212100@gmail.com>
2024-05-19 22:02:02 +0500

    added light and dark theme and login and register pages

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  final void Function()? onTap;
+  const RegisterPage({super.key, required this.onTap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.lock_open_rounded,
+            size: 100,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          const SizedBox(height: 25),
+          Text(
+            "Let's create an Account for you",
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          const SizedBox(height: 25),
+          MyTextField(
+            controller: emailController,
+            hintText: "Email",
+            obscureText: false,
+          ),
+          const SizedBox(height: 10),
+          MyTextField(
+            controller: passwordController,
+            hintText: 'Password',
+            obscureText: true,
+          ),
+          const SizedBox(height: 10),
+          MyTextField(
+            controller: confirmPasswordController,
+            hintText: 'Confirm password',
+            obscureText: true,
+          ),
+          const SizedBox(height: 25),
+          MyButton(
+            text: "Sign Up",
+            onTap: () {},
+          ),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already have an account Login In?",
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+              const SizedBox(width: 4),
+              GestureDetector(
+                onTap: widget.onTap,
+                child: Text(
+                  "Login now",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
